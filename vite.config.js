6e92a6da6696cac857e9804b71c4c669dfee6196
Giovanni Filardo <gfilardo@gmail.com>
2026-07-14 20:07:48 +0200

    build: configure single-file build and bump version to 0.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { qrcode } from 'vite-plugin-qrcode'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   base: '/agm/',
   plugins: [
     basicSsl(),
-    qrcode()
+    qrcode(),
+    viteSingleFile()
   ]
 })
